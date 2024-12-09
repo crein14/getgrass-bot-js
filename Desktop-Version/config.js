766daea653685ee004eb271a397bab91f8abc070
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-09 22:06:03 +0700

    Adding Desktop Version on the main script
    You can also check in release section.

---
+++ Desktop-Version/config.js
@@ -0,0 +1,9 @@
+// configuration
+const config = {
+    UserAgent : 'xxxxxxxxxxxxxx', // Replace with Your useragent
+    userId: 'xxxxxxxxxxxxxx',  // Replace with Your User ID HERE
+    proxyFile: 'proxy.txt' // your Path to Proxy.txt file
+    // format proxies is => socks://username:pass@ip:port
+};
+
+module.exports = config;
